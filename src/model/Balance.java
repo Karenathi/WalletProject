23b4c10a780f1c801fc3487bd48b71099c111852
velvet_vixen <hei.karenandriantasy@gmail.com>
2023-12-08 07:35:59 +0300

    feat : equals, hashCode, toString method for balance

--- src/model/Balance.java
+++ src/model/Balance.java
@@ -1,21 +1,31 @@
 package model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Balance {
+    private int id;
     private double value;
     private LocalDateTime datetime;
 
-    // Constructeurs, getters et setters
-    public Balance() {}
 
-    public Balance(double value, LocalDateTime datetime) {
+    // Constructeurs, getters et setters
+    public Balance(int id,double value, LocalDateTime datetime) {
         this.value = value;
         this.datetime = datetime;
     }
 
+
+    public int getId() {
+        return id;
+    }
+
     public double getValue() {
         return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setValue(double value) {
@@ -28,6 +38,31 @@ public class Balance {
 
     public void setDatetime(LocalDateTime datetime) {
         this.datetime = datetime;
+    }
+
+    // Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Balance that)) return false;
+        return getId() == that.getId() &&
+                Double.compare(that.getValue(), getValue()) == 0 &&
+                Objects.equals(getDatetime(), that.getDatetime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue(), getDatetime());
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "Balance{" +
+                "id=" + id +
+                ", value=" + value +
+                ", datetime=" + datetime +
+                '}';
     }
 }
 
