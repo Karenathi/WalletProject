126ce9514c67c21d3d1f8bce755b34f0790bf562
HRojotiana <hei.rojotiana@gmail.com>
2023-12-15 16:25:08 +0300

    fix: update sql function get_category_sum

--- src/functions/sql_scripts/get_category_sum.sql
+++ src/functions/sql_scripts/get_category_sum.sql
@@ -1,12 +1,16 @@
-CREATE OR REPLACE FUNCTION get_category_sum(account_id INT, start_date TIMESTAMP, end_date TIMESTAMP)
-RETURNS TABLE (restaurant DECIMAL, salaire DECIMAL) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        COALESCE(SUM(CASE WHEN category.name = 'Restaurant' THEN transaction.amount ELSE 0 END), 0) AS restaurant,
-        COALESCE(SUM(CASE WHEN category.name = 'Salaire' THEN transaction.amount ELSE 0 END), 0) AS salaire
-    FROM
-        category
-    LEFT JOIN transaction ON category.id = transaction.category_id AND transaction.account_id = account_id AND transaction.date BETWEEN start_date AND end_date;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION get_category_sum(IN bank_account_id INT, IN start_date TIMESTAMP, IN end_date TIMESTAMP)
+    RETURNS TABLE (category_name VARCHAR(100), category_amount_total DECIMAL) AS
+    $$
+    BEGIN
+        RETURN QUERY
+        SELECT
+        category.name,
+        COALESCE(SUM(CASE WHEN transactions.category_id = category.id THEN amount ELSE 0 END),0) AS category_amount_total
+        FROM category
+        LEFT JOIN transactions ON transactions.category_id = category.id
+        WHERE account = bank_account_id
+        AND transactions.date BETWEEN start_date AND end_date
+        GROUP BY category.name;
+    END;
+    $$
+    LANGUAGE plpgsql;
